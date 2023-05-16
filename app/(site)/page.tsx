80ddb0019ac4d2400baea319249387ccddf73916
Scott <scott@scotts-mbp.lan>
2023-05-16 14:47:15 -0700

    fixed errors

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,4 @@
+import FBPost from "@/components/ui/FBPost";
 import PageContainer from "@/components/ui/PageContainer";
 import PageHero from "@/components/ui/PageHero";
 import PageWrapper from "@/components/ui/PageWrapper";
@@ -12,17 +13,19 @@ interface FbData {
 }
 
 const HomePage = async () => {
-  // const res = await fetch(`${process.env.FB_URL}`);
-  // const posts = await res.json();
-
+  const res = await fetch(
+    `https://graph.facebook.com/v16.0/240300483115456/posts?fields=full_picture,created_time,updated_time,message,permalink_url&transport=cors&access_token=${process.env.NEXT_PUBLIC_FB_KEY}`
+  );
+  const posts = await res.json();
   return (
     <PageWrapper>
       <PageHero title="Latest News" subtitle="From our Facebook page" />
       <PageContainer className="flex flex-col items-center">
         <h1>Posts</h1>
-        {/* {posts.data.map((post: FbData) => (
+
+        {posts?.data.map((post: FbData) => (
           <FBPost key={post.id} post={post} />
-        ))} */}
+        ))}
       </PageContainer>
     </PageWrapper>
   );
